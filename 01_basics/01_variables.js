5e253709f8bfb755b87449e1611a006969559f7d
Samyak Hinge <81359739+Samyak214@users.noreply.github.com>
2023-12-11 19:45:05 +0000

    01_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 13334
+let accountEmail = "samyak@gmail.com"
+var accountPassword ="12345"
+accountcity="Latur"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Samyak");
+//code readability should be high
+
+let name="Samyak"
+let age=18
+let isLoggedIn= false
+let state;
+
+
+//number => 2^53
+//bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+
+//object
+console.log(typeof undefined );
